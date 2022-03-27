0e41c768b9ae62066ec463a8dd51826b691ef56c
Boodleboy <jhm16c@my.fsu.edu>
2022-03-27 11:57:03 -0400

    Add closing scripts for current session and all sessions

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ DEST=~/bin
 CONFIG=~/.config
 
 cp scripts/tide-new-win scripts/tide-open $DEST
+cp scripts/tide-close scripts/tide-close-all $DEST
 cp config/tidelfrc $CONFIG/lf/tidelfrc
 cp config/tidetmux.conf $CONFIG/tmux/tidetmux.conf
 cp config/nested.conf $CONFIG/tmux/nested.conf
