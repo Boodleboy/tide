033b24f46caac0bf5889f735ee46f1d3f36f28b4
Boodleboy <jhm16c@my.fsu.edu>
2022-03-29 16:44:55 -0400

    Add restart script to install, and organize script list

--- install.sh
+++ install.sh
@@ -3,8 +3,16 @@
 DEST=~/bin
 CONFIG=~/.config
 
-cp scripts/tide-new-win scripts/tide-open $DEST
-cp scripts/tide-close scripts/tide-close-all $DEST
+SCRIPTS="tide-new-win \
+	tide-open \
+	tide-close \
+	tide-close-all \
+	tide-restart \
+	"
+
+cd scripts
+cp $SCRIPTS $DEST
+cd ..
 cp config/tidelfrc $CONFIG/lf/tidelfrc
 cp config/tidetmux.conf $CONFIG/tmux/tidetmux.conf
 cp config/nested.conf $CONFIG/tmux/nested.conf
