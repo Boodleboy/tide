2cae21687367257b27358abc1549028491eedb55
Boodleboy <jhm16c@my.fsu.edu>
2022-04-04 21:52:34 -0400

    Improve opening flow.
    'tide' will open a new terminal, while tide-open will now only
    start the tmux session. tide will prompt user to pick a starting
    directory using lf if not given one

--- install.sh
+++ install.sh
@@ -3,16 +3,19 @@
 DEST=~/bin
 CONFIG=~/.config
 
-SCRIPTS="tide-new-win \
+SCRIPTS="tide \
+	tide-new-win \
 	tide-open \
 	tide-close \
 	tide-close-all \
 	tide-restart \
+	tide-dir-picker
 	"
 
 cd scripts
 cp $SCRIPTS $DEST
 cd ..
+cp config/tidelfpickerrc $CONFIG/lf/tidelfpickerrc
 cp config/tidelfrc $CONFIG/lf/tidelfrc
 cp config/tidetmux.conf $CONFIG/tmux/tidetmux.conf
 cp config/nested.conf $CONFIG/tmux/nested.conf
